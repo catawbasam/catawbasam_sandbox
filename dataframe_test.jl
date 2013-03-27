1d6a0aa4512384bc5136fe44b6399ebb4eb0c4f0
Keith <keithc@mitre.org>
2013-03-26 23:08:58 -0400

    more grouping and merging from DataFrames docs and examples

--- dataframe_test.jl
+++ dataframe_test.jl
@@ -1,14 +1,10 @@
 #To install DataFrames.jl, use the following:
 #Pkg.add("DataFrames")
-# to run from console: 
-# julia>include("dataframe_test.jl")
+# to run from console:  julia>include("dataframe_test.jl")
 
-# Load DataFrames
-#require("DataFrames")
 using DataFrames
 
-
-# DataArray is analogous to pandas Series
+println("DataArray is analogous to pandas Series")
 da=DataArray([5:8])  #build from Array
 length(da)
 sum(da)
@@ -17,10 +13,11 @@ std(da)
 da .> 7  #array of Bools  true/false
 da[da.>7] #filter on condition
 
+#see https://github.com/HarlanH/DataFrames.jl/blob/master/demo/demo3.jl
 # Load a CSV file into a DataFrame.
 df = read_table("toy_example.csv")
 
-# Basic ways to look at a DataFrame
+println("\nBasic ways to look at a DataFrame")
 print(df)
 head(df,3)
 tail(df)
@@ -38,9 +35,9 @@ df[1]     # refer to col by index number
 df[1:3]   # range of cols
 df[["A","B"]]  # list of cols
 
-# Look at the data structure.
+println("\nLook at the data structure.")
 dump(df)  # dump() is like R's str().
-idump(df)  # Look at the internal data structure.
+#idump(df)  # Look at the internal data structure.
 
 # Basic referencing and slicing
 # singleton references return a basic element; slices are DataFrames
@@ -60,8 +57,60 @@ df["E"]= 42
 df["E"]= NA
 write_table("toy2.csv", df)
 
+# Select all rows where column A is greater than 4.0
+# Element-wise operators in Julia usually have a leading "."
+df[:( A .> 4.0 ), :]
+
+# Make a new column using within
+df2 = within(df, :( D = A + C ))
+
+# This is similar, but now changes apply directly to df
+within!(df, quote
+    D = A + C
+    E = A + sum(C)
+end)
+
+
 # add rows/cols
 # hcat, vcat, hbind, cbind()
 
-#aggregation
+println("\nGrouping and aggregation")
 grpB = groupby(df,"B")
+
+# from https://github.com/HarlanH/DataFrames.jl/blob/master/demo/examples.jl
+using RDatasets
+iris = RDatasets.data("datasets", "iris")
+
+unique(iris["Species"])
+stack(iris, ["Petal.Length", "Petal.Width"])
+
+for df in groupby(iris, "Species")
+    println("A DataFrame with $(nrow(df)) rows")
+    #print(head(df,2))
+end
+
+#
+# The | (pipe) operator for GroupedDataFrame's allows you to run
+# simple functions on the columns of the induced DataFrame's.
+#
+# You pass a simple function by producing a symbol with its name.
+#
+
+print(groupby(iris, "Species") | :mean)
+
+#https://github.com/HarlanH/DataFrames.jl/blob/master/demo/demo4.jl
+# Group by a and b:
+by(df, ["a", "b"], :( x_sum = sum(x); x_len = length(x) ))
+
+
+print("\nMerge and Join")
+flowers = DataFrame()
+flowers["Species"] = ["virginica", "versicolor", "setosa", "daisy"]
+flowers["PrimaryColor"] = ["purplish", "purple", "purple", "yellow"]
+
+merge(iris, flowers, "Species", "inner")
+merge(iris, flowers, "Species", "left")
+merge(iris, flowers, "Species", "right")
+merge(iris, flowers, "Species", "outer")
+
+print("Done")
